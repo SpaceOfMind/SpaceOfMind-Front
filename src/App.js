f7880d99b66814912c5bf582a8293f5256413905
Hyemin Lee <cotloz88@gmail.com>
2023-07-16 02:25:29 +0900

    feat: cookie 파싱을 위한 모듈 및 util 추가

--- src/App.js
+++ src/App.js
@@ -7,18 +7,21 @@ import Authentication from './pages/Authentication/Authentication';
 import theme from './theme';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignUp from './pages/SignUp/SignUp';
+import { CookiesProvider } from 'react-cookie';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box w="100%" h="100vh" bgImage="/background.jpg" bgSize="cover">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/login" element={<Authentication />} />
-            <Route path="/signUp" element={<SignUp />} />
-          </Routes>
-        </BrowserRouter>
-      </Box>
+      <CookiesProvider>
+        <Box w="100%" h="100vh" bgImage="/background.jpg" bgSize="cover">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/login" element={<Authentication />} />
+              <Route path="/signUp" element={<SignUp />} />
+            </Routes>
+          </BrowserRouter>
+        </Box>
+      </CookiesProvider>
     </ChakraProvider>
   );
 }
