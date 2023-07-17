f6dbfb063bd9a37c3b5878bff64c7885998250e6
ihchaeryu <120316174+ihchaeryu@users.noreply.github.com>
2023-07-17 15:53:16 +0900

    Feature/around merge with develop (#3)

---
+++ src/components/Satellites/Satellite.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Box } from '@chakra-ui/react';
+import './Satellite.scss';
+
+export const Satellite1 = () => {
+    return (
+        <Box 
+            className="container satellite-1"
+            w='340px'
+            h='180px'
+        />
+    );
+};
+
+export const Satellite2 = () => {
+    return (
+        <Box 
+            className="container satellite-2"
+            w='340px'
+            h='180px'
+        />
+    );
+};
+
+export const Satellite3 = () => {
+    return (
+        <Box 
+            className="container satellite-3"
+            w='340px'
+            h='180px'
+        />
+    );
+};
+
+export const Satellite4 = () => {
+    return (
+        <Box 
+            className="container satellite-4"
+            w='340px'
+            h='180px'
+        />
+    );
+};
